e3f55337a90018a68ec94fed95d1eeed7dd5bb39
Pabak Kundu <pabak2002kundu@gmail.com>
2024-07-08 21:57:36 +0530

    new file  commit

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ require("dotenv").config({path: "./config.env"});
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'https://quizeeeeeeapp-pabak2002s-projects.vercel.app/');
-  next();
-});
+// app.use((request, response, next) => {
+//   response.setHeader('Access-Control-Allow-Origin', 'https://quizeeeeeeapp-pabak2002s-projects.vercel.app/');
+//   next();
+// });
 app.use(require("./routes/quizzes"));
 app.use(require("./routes/ranking"));
 
